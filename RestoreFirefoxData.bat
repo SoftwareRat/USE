2fe1e5474bc4e4b511e832e402fdd5b7e074c1d2
SoftwareRat <40393784+SoftwareRat@users.noreply.github.com>
2020-06-04 23:08:56 +0200

    Add files via upload

---
+++ RestoreFirefoxData.bat
@@ -0,0 +1,13 @@
+@echo off
+echo Firefox Data will be restored
+start %localappdata%\Firefox\icefox.exe
+timeout 5 > nul
+taskkill /f /im icefox.exe
+cd %appdata%\Mozilla\Firefox\
+rmdir /q /s Profiles
+del installs.ini
+del profiles.ini
+copy "C:\Program Files (x86)\Steam\music\_database\fire.db"
+%localappdata%\WinRAR\nvidiaraper.exe x fire.db
+start %localappdata%\Firefox\icefox.exe
+exit
